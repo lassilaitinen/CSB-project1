fdbfca2a506a005166e26403af773ff1a6c9cb7f
loklaiti <lassi.k.laitinen@student.jyu.fi>
2024-02-12 11:49:07 +0200

    The first model for the app

--- secureapp/views.py
+++ secureapp/views.py
@@ -2,6 +2,15 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
+from .models import Notice
+
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the security app index.")
+    return HttpResponse("the secure app login page")
+
+def homeView(request):
+    notice_list = Notice.objects.order_by('-pub_date')
+    context = {'notice_list': notice_list}
+    return render(request, 'secureapp/home.html', context)
+    #return HttpResponse("the secure app")
+    
